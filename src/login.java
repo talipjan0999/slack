ef58eb41a6aa021b4b909da1dcdc42a22c26d0a9
talipjan0999 <61458968+talipjan0999@users.noreply.github.com>
2020-05-12 18:23:44 +0200

    Update login.java

--- src/login.java
+++ src/login.java
@@ -3,5 +3,9 @@ import java.util.Arrays;
 public class login {
     public static void main(String[] args) {
         System.out.println("args = " + Arrays.deepToString(args));
+        
+        
+        bu kunularmu yahxi bop kala
+            
     }
 }
