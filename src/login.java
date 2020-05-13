eb40c29c4c14485382fb4a3a7dd4ecbe585ae066
talipjan0999 <talipjan1208@Gmail.com>
2020-05-13 16:34:56 +0200

    update login.java

--- src/login.java
+++ src/login.java
@@ -3,9 +3,13 @@ import java.util.Arrays;
 public class login {
     public static void main(String[] args) {
         System.out.println("args = " + Arrays.deepToString(args));
-        
-        
-        bu kunularmu yahxi bop kala
+
+
+                instanceof i=0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+
+        }
             
     }
 }
