c802218a32943811f145354a135fa64a2f4d6f00
talipjan0999 <61458968+talipjan0999@users.noreply.github.com>
2020-05-13 16:57:20 +0200

    Update creatchannel.java

--- src/day_1/creatchannel.java
+++ src/day_1/creatchannel.java
@@ -3,5 +3,11 @@ package day_1;
 public class creatchannel {
     public static void main(String[] args) {
         System.out.println("user is able to creat channel");
+        
+        
+        
+        
+        
+        //this is for cinfilict class 
     }
 }
