310234eafe9a60c57279ed49279a85b89e203ed3
talipjan0999 <talipjan1208@Gmail.com>
2020-05-13 16:56:05 +0200

    update creatchannel.java

--- src/day_1/creatchannel.java
+++ src/day_1/creatchannel.java
@@ -1,7 +1,15 @@
 package day_1;
 
+import java.util.Arrays;
+
 public class creatchannel {
     public static void main(String[] args) {
         System.out.println("user is able to creat channel");
+
+
+
+        for (3>2){
+            System.out.println("args = " + Arrays.deepToString(args));
+        }
     }
 }
